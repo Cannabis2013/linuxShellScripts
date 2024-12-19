451011f37608cf426e572f42031a0607edebfedb
Martin Hansen <mh@debian.mh>
2024-12-19 11:55:09 +0100

    Added checks on avr hex dump

--- to_hex.sh
+++ to_hex.sh
@@ -1,10 +1,26 @@
 FILENAME=$1
 
-if [ ! -z FILENAME ]; then
-    FILENAME="output"
+if [ -z $FILENAME ]; then
+    echo "No file provided"
+    return
+elif [ -s $FILENAME ];then
+    echo "File not found"
+    return
 fi
 
-OUTPUT=$FILENAME.o
+str="${FILENAME}"
+binaryName=()
+
+length=${#str}
+for (( i = 0; i < length; i++ )); do
+    char="${str:$i:1}"
+    if [ $char == "." ];then
+        break
+    fi
+    binaryName+=$char
+done
+
+OUTPUT=$binaryName.o
 
 avr-gcc -c *.c -o $OUTPUT
 
